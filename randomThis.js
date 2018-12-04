dda9f32c9c53ed1dc3c00f113982663645a48ba3
LanFP888 <41882036+LanFP888@users.noreply.github.com>
2018-12-04 18:59:24 -0500

    refactor randomThis.js

--- randomThis.js
+++ randomThis.js
@@ -9,28 +9,32 @@ module.exports = function randomThis(){
         if (err) {
           return console.log(err);
         }
-        let randomArr = data.split('\r\n')
-        let randomIndex = Math.floor(Math.random() * randomArr.length)
-        let randomCommand = randomArr[randomIndex]
-        let command = randomCommand.split(',')
-        let cm = command[0]
-        let param = command[1].replace (/(^")|("$)/g, '')
-
-        logToFile(`\r\n${cm} ${param}\r\n`)
-
-        switch (cm){
-            case ('concert-this'):
-                concertThis(param)
-                break;
-            case ('movie-this'):
-                movieThis(param)
-                break;
-            case ('spotify-this'):
-                spotifyThis(param)
-                break;
-            default:
-                logToFile('\r\ninvalid command, please check your random.txt file\r\n')
-                console.log("invalid command, please check your random.txt file")
-        }
+        doRandom(data)
     })
+}
+
+function doRandom(data){
+    let randomArr = data.split('\n')
+    let randomIndex = Math.floor(Math.random() * randomArr.length)
+    let randomCommand = randomArr[randomIndex]
+    let command = randomCommand.split(',')
+    let cm = command[0]
+    let param = command[1].replace (/(^")|("$)/g, '')
+
+    logToFile(`\r\n${cm} ${param}\r\n`)
+
+    switch (cm){
+        case ('concert-this'):
+            concertThis(param)
+            break;
+        case ('movie-this'):
+            movieThis(param)
+            break;
+        case ('spotify-this'):
+            spotifyThis(param)
+            break;
+        default:
+            logToFile('\r\ninvalid command, please check your random.txt file\r\n')
+            console.log("invalid command, please check your random.txt file")
+    }
 }
